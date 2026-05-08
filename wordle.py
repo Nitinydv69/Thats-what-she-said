1003a931461d702c655e834a6b96247f5ca810c4
Nitinydv69 <142815678+Nitinydv69@users.noreply.github.com>
2026-05-08 14:10:14 +0200

    guess recovery and hint system
    added hint system for easy gameplay and the number of guesses remain same if the length of the sentence is not equal to the sentence

--- wordle.py
+++ wordle.py
@@ -14,10 +14,14 @@ print("THE GAME BEGINS")
 print("GUESS WHAT SHE SAID")
 print(twss)
 # number of guesses 6
-for guess_num in range(1, 7):
+guess_num = 1
+
+while guess_num <= 6:
 
     guess = input(f"guess{guess_num}:").upper().split()
     # word count validation
+    # nested while for replay tries
+    #############################
     if len(guess) > len(she_said):
         print("Too many words Solider")
         continue
@@ -27,6 +31,7 @@ for guess_num in range(1, 7):
     if guess == she_said:
         print("\n🎉 Congratulations! That's what she said")
         break
+    guess_num += 1
     # store the results
     result = [None] * len(she_said)
     # duplicate handling ( suggested by gpt I dont give a fuckl!!!)
@@ -36,7 +41,7 @@ for guess_num in range(1, 7):
         if guess[i] == she_said[i]:
             result[i] = "🟩"
             remaining[i] = None
-    # wrong place
+# wrong place
     for i in range(len(guess)):
         if result[i] is None:
             if guess[i] in remaining:
@@ -52,6 +57,18 @@ for guess_num in range(1, 7):
     for word, color in zip(guess, result):
         print(f"{word:<20},{color}")
 
+
+# hint system :
+    use_hint = input("\nDo you want a hint? y/n").lower()
+    if use_hint == "y":
+        if guess_num == 2:
+            print(f"The first word is:{she_said[0]}")
+        elif guess_num == 3:
+            print(f"The last word is {she_said[-1]}")
+        elif guess_num == 4:
+            print(f"Cmon there is {she_said[-2]} in it")
     # lose
 else:
     print("\nAh You don't know")
+    print(f"\n She said {twss}")
+    print("cause we need 69 lines of code")
